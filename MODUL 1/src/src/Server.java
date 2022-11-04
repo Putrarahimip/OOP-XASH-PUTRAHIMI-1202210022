693a2d3eb8e47b12b19f90acaa1d72c1ed794800
Putrarahimip <91724920+Putrarahimip@users.noreply.github.com>
2022-11-04 16:47:05 +0700

    Add files via upload

---
+++ MODUL 1/src/src/Server.java
@@ -0,0 +1,35 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        
+        // TODO Create Database
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Database");
+
+        // TODO Create Menu
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Menu");
+
+        // TODO Insert Menu to Database
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Menu to Database");
+
+        // TODO Display Main Menu
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Main Menu");
+
+        // TODO Create User
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nama : " + (User.nama));
+
+        // Display Menu
+        System.out.println("Selamat Datang di Restoran EAD");
+        System.out.println("Silahkan Register Terlebih Dahulu");
+        System.out.println("=================================");
+
+    }
+}
